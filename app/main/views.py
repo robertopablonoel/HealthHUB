da89d2bd53c8bd9a1765ff39846b84daadcc6804
Roberto Noel <robertonoel@Robertos-MacBook-Pro.local>
2019-11-10 19:51:15 +0800

    hi

--- app/main/views.py
+++ app/main/views.py
@@ -10,17 +10,9 @@ from ..models import User
 
 @main.route('/', methods = ['GET','POST'])
 def index():
-    loginform = LoginForm()
-    if loginform.validate_on_submit():
-        user = User.query.filter_by(email=loginform.email.data).first()
-        if user is not None and user.verify_password(loginform.password.data):
-            login_user(user, loginform.remember_me.data)
-            #if login form was presented to the user to prevent
-            #unauthorized access to a protected URL, Flask-Login
-            #Saves the original URL in the next query string
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid email or password.')
-    return render_template('index.html', login_form = loginform)
+    return render_template('index.html')
 
+@main.route('/home', methods = ['GET','POST'])
+@login_required
 def home():
     return render_template('home.html')
