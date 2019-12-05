dc01011a41a52956c654ff036c1c49c37e1bb0c1
jackschulz <js9205@nyu.edu>
2019-12-05 21:51:05 +0800

    closer to everything working

--- app/profile/views.py
+++ app/profile/views.py
@@ -2,9 +2,9 @@ from flask import render_template, redirect, request, url_for, flash
 from .forms import NewPrescriptionForm
 from datetime import datetime, date
 from flask_login import login_user, login_required, logout_user, current_user
-from ..models import Prescription, User
+from ..models import Prescription, User, Patient
 from .. import db
-from . import prescript
+from . import profile
 from flask_jsonpify import jsonify
 import re
 
@@ -12,12 +12,16 @@ import re
 @profile.route('/patient/<user_id>', methods = ['GET','POST'])
 @login_required
 def patient(user_id):
+    print("yo yo you got ya boi")
     patient_user = User.query.filter_by(user_id = user_id).first_or_404()
     patient = Patient.query.filter_by(user_id = user_id).first_or_404()
     precription = Prescription.query.filter_by(patient_id = user_id).all()
+    return render_template('profile/patient.html', patient_user = patient_user, patient = patient, prescription = prescription)
 
-    if patient is not None:
-        return render_template('profile/patient.html', patient_user = patient_user, patient = patient, prescription = prescription)
+@profile.route('/search', methods = ['GET', 'POST'])
+@login_required
+def search():
+    return render_template('profile/search_patient.html')
 
 @profile.route('/autocomplete', methods = ['GET'])
 @login_required
