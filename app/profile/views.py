c2a39e7b16f40807824e8e0222754c7d0fd5b4e4
jackschulz <js9205@nyu.edu>
2019-12-05 20:54:14 +0800

    partial update

---
+++ app/profile/views.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, request, url_for, flash
+from .forms import NewPrescriptionForm
+from datetime import datetime, date
+from flask_login import login_user, login_required, logout_user, current_user
+from ..models import Prescription, User
+from .. import db
+from . import prescript
+from flask_jsonpify import jsonify
+import re
+
+
+@profile.route('/patient/<user_id>', methods = ['GET','POST'])
+@login_required
+def patient(user_id):
+    patient_user = User.query.filter_by(user_id = user_id).first_or_404()
+    patient = Patient.query.filter_by(user_id = user_id).first_or_404()
+    precription = Prescription.query.filter_by(patient_id = user_id).all()
+
+    if patient is not None:
+        return render_template('profile/patient.html', patient_user = patient_user, patient = patient, prescription = prescription)
