3bf58b26d336df01af28a7e021bbab51766c902c
Roberto Noel <robertonoel@Robertos-MacBook-Pro.local>
2019-12-07 01:32:12 +0800

    forum updates

---
+++ migrations/versions/fdf1a577140f_poopidy_scoop.py
@@ -0,0 +1,24 @@
+"""poopidy scoop
+
+Revision ID: fdf1a577140f
+Revises: 224ea15bce17, a7fdeff86371
+Create Date: 2019-12-06 19:26:55.626342
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fdf1a577140f'
+down_revision = ('224ea15bce17', 'a7fdeff86371')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
