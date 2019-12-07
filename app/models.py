b20a9576ac1c2ff61b59f9fb1d06fe6cba840c0a
jackschulz <js9205@nyu.edu>
2019-12-07 21:53:27 +0800

    missed 4 hours of work and all i got was tired

--- app/models.py
+++ app/models.py
@@ -219,7 +219,9 @@ class Prescription(db.Model):
     date_prescribed = db.Column(db.Date, nullable = False)
     expir_date = db.Column(db.Date, nullable = False)
     description = db.Column(db.Text, nullable = True)
-    #active = db.Column(db.Boolean, default = True)
+    active = db.Column(db.Boolean, default = True)
+    notify = db.Column(db.Boolean, default = True)
+
 
 class Physician_schedule(db.Model):
     event_id = db.Column(db.Integer, primary_key = True)
